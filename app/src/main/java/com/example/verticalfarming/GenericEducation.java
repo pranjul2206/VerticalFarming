1baa84beda1d524c1285ff44d19946dcc9026851
pranjul2206 <pranjulmishra10112206@gmail.com>
2019-07-23 22:10:28 +0530

    GenericEducation step 1 MAIN TOPICS

--- app/src/main/java/com/example/verticalfarming/GenericEducation.java
+++ app/src/main/java/com/example/verticalfarming/GenericEducation.java
@@ -1,39 +1,80 @@
 package com.example.verticalfarming;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
+import android.animation.ArgbEvaluator;
 import android.os.Bundle;
-
-import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.data.PieData;
-import com.github.mikephil.charting.data.PieDataSet;
-import com.github.mikephil.charting.data.PieEntry;
-import com.github.mikephil.charting.utils.ColorTemplate;
-
+import android.util.Log;
 import java.util.ArrayList;
 import java.util.List;
 
 public class GenericEducation extends AppCompatActivity {
-    float rainfall[]={98.8f,123.8f,161.6f,24.2f,52f,58.2f,78.4f,203.4f,240.2f,159.7f,13.9f,35.1f};
-    String monthName[]={"jan","feb","mar","apr","may","june","july","aug","sept","oct","nov","dec"};
+
+    ViewPager viewPager;
+    GenericEducationAdapter adapter;
+    List<GenericEducationModel> models;
+    Integer[] colors = null;
+    ArgbEvaluator argbEvaluator = new ArgbEvaluator();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Log.i("PRANJUL1", "balleballe1");
         setContentView(R.layout.activity_generic_education);
-        setupPieChart();
+        //Log.i("PRANJUL2", "balleballe2");
+        models = new ArrayList<>();
+        models.add(new GenericEducationModel(R.drawable.zbrochure, "Hydroponics", "Hydroponics is a subset of hydroculture, which is a method of growing plants without soil by instead using mineral nutrient solutions in a water solvent."));
+        models.add(new GenericEducationModel(R.drawable.zsticker, "Aeroponics", "Aeroponics is the process of growing plants in an air or mist environment without the use of soil or an aggregate medium (known as geoponics)."));
+        models.add(new GenericEducationModel(R.drawable.zposter, "Aquaponics", "Aquaponics refers to any system that combines conventional aquaculture with hydroponics in a symbiotic environment. "));
+        models.add(new GenericEducationModel(R.drawable.znamecard, "Cocoppeat", "Coir, or coconut fibre, is a natural fibre extracted from the husk of coconut and used in products such as floor mats, doormats, brushes and mattresses."));
+        //Log.i("PRANJUL3", "balleballe3");
+        adapter = new GenericEducationAdapter(models, this);
+
+        viewPager = findViewById(R.id.viewPager);
+        viewPager.setAdapter(adapter);
+        viewPager.setPadding(130, 0, 130, 0);
+
+        Integer[] colors_temp = {
+                getResources().getColor(R.color.color1),
+                getResources().getColor(R.color.color2),
+                getResources().getColor(R.color.color3),
+                getResources().getColor(R.color.color4)
+        };
+
+        colors = colors_temp;
+
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+                if (position < (adapter.getCount() -1) && position < (colors.length - 1)) {
+                    viewPager.setBackgroundColor(
+
+                            (Integer) argbEvaluator.evaluate(
+                                    positionOffset,
+                                    colors[position],
+                                    colors[position + 1]
+                            )
+                    );
+                }
+
+                else {
+                    viewPager.setBackgroundColor(colors[colors.length - 1]);
+                }
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
     }
-    private void setupPieChart(){
-        List<PieEntry> pieEntries=new ArrayList<>();
-        for(int i=0;i<rainfall.length;i++)
-        {
-            pieEntries.add(new PieEntry(rainfall[i],monthName[i]));
-        }
-        PieDataSet dataset=new PieDataSet(pieEntries,"RainFall in Bangalore");
-        dataset.setColors(ColorTemplate.MATERIAL_COLORS);
-        PieData data=new PieData(dataset);
-        PieChart chart=(PieChart)findViewById(R.id.bangalorepiecharts);
-        chart.setData(data);
-        chart.invalidate();
     }
-}
+
---
+++ app/src/main/java/com/example/verticalfarming/GenericEducationAdapter.java
@@ -0,0 +1,66 @@
+package com.example.verticalfarming;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import java.util.List;
+
+public class GenericEducationAdapter extends PagerAdapter {
+    private List<GenericEducationModel> models;
+    private Context context;
+
+    public GenericEducationAdapter(List<GenericEducationModel> models, Context context) {
+        this.models = models;
+        this.context = context;
+    }
+
+    public int getCount() {
+        return models.size();
+    }
+
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view.equals(object);
+    }
+
+    @NonNull
+    public Object instantiateItem(@NonNull ViewGroup container, final int position) {
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        View view = layoutInflater.inflate(R.layout.item, container, false);
+
+        ImageView imageView;
+        TextView title, desc;
+
+        imageView = view.findViewById(R.id.image);
+        title = view.findViewById(R.id.title);
+        desc = view.findViewById(R.id.desc);
+
+        imageView.setImageResource(models.get(position).getImage());
+        title.setText(models.get(position).getTitle());
+        desc.setText(models.get(position).getDesc());
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, BasicHome.class);
+                intent.putExtra("param", models.get(position).getTitle());
+                context.startActivity(intent);
+                // finish();
+            }
+        });
+
+        container.addView(view, 0);
+        return view;
+    }
+
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View)object);
+    }
+}
